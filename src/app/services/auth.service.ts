0af9b963dfe3a77e155e758dc5734193a8a5368a
SuSu <susu@MacBook-Pro-de-SuSu.local>
2019-12-31 13:28:15 -0800

    app book with angular + firebase

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  // créer un nouvel utilisateur.
+  createNewUSer(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+//la connexion d'un utilisateur déja existant
+  signInUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+//deconnexion d'un user
+  signOutUser() {
+    firebase.auth().signOut();
+  }
+}
